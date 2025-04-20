429d245256c45768be61c77766901c286f03db33
tmenguy <thomas.menguy@gmail.com>
2025-04-20 17:32:19 +0200

    put back HA changes

--- custom_components/renault/const.py
+++ custom_components/renault/const.py
@@ -8,6 +8,8 @@ CONF_LOCALE = "locale"
 CONF_KAMEREON_ACCOUNT_ID = "kamereon_account_id"
 
 # normal number of allowed calls per hour to the API
+# for a single car and the 7 coordinators, 60 is a scan every 7mn
+# We can safely bump it to 100 with the smoothing
 MAX_CALLS_PER_HOURS = 100
 
 # If throttled time to pause the updates, in seconds
